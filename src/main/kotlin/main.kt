969c95eac4bcc603bd1de4bc73928b2e33f37de8
ArtBekk <arturbekk@gmail.com>
2020-10-11 18:57:49 +0700

    Added comments for explanation

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,7 @@ fun main(args: Array<String>) {
         val words = readLine().toString().split(' ')
         run(words)
     } else run(args.toList())
-}
+}//This function purpose is solely for receiving words from the stdin or arguments and passing them to the run function
 
 fun run(input: List<String>) {
     level1(input)
@@ -37,14 +37,14 @@ fun level5(input: List<String>) {
     println("LEVEL 5:")
     for (word in input.groupingBy { it }
         .eachCount().toList()
-        .sortedWith(compareBy({ -it.second }, { it.first }))) {
+        .sortedWith(compareBy({ -it.second }, { it.first }))) /*-it.second since we need descending order*/{
         println("${word.first} ${word.second}")
     }
 }
 
 fun printOnSeparateLines(input: List<String>) {
     for (word in input) println(word)
-}
+}//Prints the string from list on separate lines. these functions exists in order to reduce the code's duplication
 
 fun printOnSeparateLines(input: Map<String, Int>) {
     for (word in input) println("${word.key} ${word.value}")
